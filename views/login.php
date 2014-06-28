81e01595ed87796a54509dc8c464885a327b8355
asifkinfroninfotech <khanasifphp@gmail.com>
2014-06-28 13:44:24 +0530

    delete all code

--- views/login.php
+++
@@ -1,8 +0,0 @@
-<div id="content">
-    <form id="login">
-     <label>Email: &nbsp;&nbsp;</label>&nbsp;&nbsp;&nbsp;<input type='text' name='email' id="email" /><br/><br />
-     <label>Password:</label><input type='password' name='password' id="password" /><br/><br />
-     <label></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='submit' id='submitok' value="Submit"  />
-     </form>
-</div>
-
